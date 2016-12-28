a2177fbf122612efd6c6cd81ea25c4d51e6138c5
myafeier <hiall@aliyun.com>
2016-12-28 22:38:47 +0800

    some update

--- main.go
+++ main.go
@@ -1,66 +1,136 @@
-package	main
+package main
 
 import (
-	"net/http"
-	"log"
 	"io/ioutil"
-	"strings"
+	"log"
+	"net/http"
 	"os/exec"
+	"strings"
 	//"os"
+	"bufio"
+	"flag"
 	"os"
 )
 
-const ConfFile string ="conf"
 var homePath string
-var cmdArray []string
+var buildArray []string
+var copyArray []string
+var buildName *string
+var copyName *string
 
 func main() {
-	log.SetFlags(log.Ldate|log.Ltime|log.Lshortfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
-	homePath=os.Getenv("AUTODHOME")
-	if homePath==""{
+	homePath = os.Getenv("AUTODHOME")
+	if homePath == "" {
 		panic("You must set AUTODHOME!")
 	}
-	bytes,err:=ioutil.ReadFile(homePath+"/"+ConfFile)
-	if err!=nil{
+
+	buildName = flag.String("b", "build.sh", "build script name")
+	copyName=flag.String("c","copy.sh","copy script name")
+	port := flag.String("port", "6000", "server port")
+	flag.Parse()
+
+	bytes, err := ioutil.ReadFile(homePath + "/" + *buildName)
+	if err != nil {
 		log.Fatal(err)
 	}
-	text:=string(bytes)
-	cmdArray=strings.Split(text,"\n")
-	log.Printf("%V",cmdArray)
 
-	http.HandleFunc("/",defaultHandler)
-	log.Println("Server Listening 9090")
+	text := string(bytes)
+	buildArray = strings.Split(text, "\n")
+	log.Printf("%V", buildArray)
 
-	log.Fatal(http.ListenAndServe(":9090",nil))
+	cBytes,err:=ioutil.ReadFile(homePath+"/"+*copyName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cText:=string(cBytes)
+	copyArray=strings.Split(cText,"\n")
+	log.Printf("%V", copyArray)
+
+	http.HandleFunc("/build", buildHandler)
+	http.HandleFunc("/copy",copyHandler)
+	http.HandleFunc("/assoc_www",assocWWWHandler)
+	log.Println("Server Listening:" + *port)
+
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
-
-func defaultHandler(w http.ResponseWriter,r *http.Request)  {
-
-	err:=r.ParseForm()
-	if err!=nil{
+func assocWWWHandler(w http.ResponseWriter,r *http.Request){
+	err := r.ParseForm()
+	if err != nil {
 		log.Println(err)
 	}
 
-
-	if r.Method=="POST"{
+	if r.Method == "POST" {
 		log.Println("Method:POST")
-	}else if r.Method=="GET"{
+	} else if r.Method == "GET" {
 		log.Println("Method:GET")
 		log.Println(r.Form)
 	}
-
-	body,err:=ioutil.ReadAll(r.Body)
-	if err!=nil{
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
 		log.Println(err)
 		w.WriteHeader(403)
 		return
 	}
-	if strings.Contains(string(body),"\"secret\": \"xxxxxx\""){
+	if !strings.Contains(string(body), "\"secret\": \"sksheixks\"") {
+		w.WriteHeader(403)
+		return
+	}
+
+	execCommand("/bin/sh", []string{homePath + "/assoc_www.sh" })
+
+}
+func copyHandler(w http.ResponseWriter, r *http.Request){
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	if r.Method == "POST" {
+		log.Println("Method:POST")
+	} else if r.Method == "GET" {
+		log.Println("Method:GET")
+		log.Println(r.Form)
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(403)
+		return
+	}
+	if strings.Contains(string(body), "\"secret\": \"xxxxxx\"") {
 		log.Println("true request!")
 	}
 
-	execCommand("/bin/sh",[]string{homePath+"/conf"})
+	execCommand("/bin/sh", []string{homePath + "/" + *copyName})
+}
+
+func buildHandler(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	if r.Method == "POST" {
+		log.Println("Method:POST")
+	} else if r.Method == "GET" {
+		log.Println("Method:GET")
+		log.Println(r.Form)
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(403)
+		return
+	}
+	if strings.Contains(string(body), "\"secret\": \"xxxxxx\"") {
+		log.Println("true request!")
+	}
+
+	execCommand("/bin/sh", []string{homePath + "/" + *buildName})
 
 	//for _,v:=range cmdArray{
 	//	cmd:=strings.Split(v," ")
@@ -69,67 +139,75 @@ func defaultHandler(w http.ResponseWriter,r *http.Request)  {
 	//	execCommand(command,params)
 	//}
 
-
 }
 
-func execCommand(command string,params []string)bool{
-	pwd,err:=os.Getwd()
-	log.Println("pwd:",pwd)
-	if strings.HasPrefix(command,"cd"){
-		err:=os.Chdir(params[0])
-		if err!=nil{
+func execCommand(command string, params []string) bool {
+	pwd, err := os.Getwd()
+	log.Println("pwd:", pwd)
+	if strings.HasPrefix(command, "cd") {
+		err := os.Chdir(params[0])
+		if err != nil {
 			log.Println(err)
 			return false
 		}
 		return true
 	}
 
+	cmd := exec.Command(command, params...)
+	log.Println("cmd:", cmd.Args)
 
-	cmd:=exec.Command(command,params...)
-	log.Println("cmd:",cmd.Args)
-
-	stdout,err:=cmd.StdoutPipe()
-	if err!=nil{
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
 		log.Println(err)
 		return false
 	}
-	stderr,err:=cmd.StderrPipe()
-	if err!=nil{
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
 		log.Println(err)
 		return false
 	}
 
-	err=cmd.Start()
-	if err!=nil{
-		log.Println("cmd start err:",err)
+	err = cmd.Start()
+	if err != nil {
+		log.Println("cmd start err:", err)
 		return false
 	}
 
-	outReader,err:=ioutil.ReadAll(stdout)
-	errReader,err:=ioutil.ReadAll(stderr)
-	if err!=nil{
-		log.Println("err:",err)
+	in := bufio.NewScanner(stdout)
+	for in.Scan() {
+		log.Println(in.Text())
+	}
+	if err := in.Err(); err != nil {
+		log.Println(err)
+	}
+
+	in2 := bufio.NewScanner(stderr)
+	for in2.Scan() {
+		log.Println(in2.Text())
+	}
+	if err := in2.Err(); err != nil {
+		log.Println(err)
+	}
+
+	//outReader, err := ioutil.ReadAll(stdout)
+	//errReader, err := ioutil.ReadAll(stderr)
+	//if err != nil {
+	//	log.Println("err:", err)
+	//	return false
+	//}
+	//
+	//if outReader != nil {
+	//	log.Println("Stdout:", string(outReader))
+	//}
+	//if errReader != nil {
+	//	log.Println("Stderr:", string(errReader))
+	//}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Println("stderr:", err)
 		return false
 	}
-
-
-	if outReader!=nil{
-		log.Println("Stdout:",string(outReader))
-	}
-	if errReader!=nil{
-		log.Println("Stderr:",string(errReader))
-	}
-
-
-	err=cmd.Wait()
-	if err!=nil{
-		log.Println("stderr:",err)
-		return false
-	}
-
-
-
-
 
 	return true
 
